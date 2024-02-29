d4cd6d60580850aafdada638b9a79e1725d6fb71
Ye Myat Moe <149359862+YeMyat144@users.noreply.github.com>
2024-02-29 12:03:15 +0700

    Add files via upload

--- ISA.py
+++ ISA.py
@@ -1,6 +1,56 @@
+class InstructionSet:
+    def __init__(self, step, opcode, register, clkcyc, operand=""):
+        self.step = step
+        self.opcode = opcode
+        self.register = register
+        self.clkcyc = clkcyc
+        self.value = int(register.get_reg_val()) 
+        self.operand = operand
+
+    def get_step(self):
+        return self.step
+
+    def get_opcode(self):
+        return self.opcode
+
+    def get_register(self):
+        return self.register
+
+    def get_clkcyc(self):
+        return self.clkcyc
+
+    def get_operand(self):
+        return self.operand
+
+    def get_value(self):
+        return self.value
+
+    def five_bit_opcode(self):
+        opcode_map = {
+            "mov": "00001",
+            "add": "00010",
+            "mul": "00100",
+            "sub": "00011",
+            "div": "00101"
+        }
+        return opcode_map.get(self.opcode, "end 0 0")
+
+    def to_16_bit_val(self):
+        if self.value >= 0:
+            return format(self.value, '016b')
+        else:
+            return bin(self.value & 0b1111111111111111)[2:].zfill(16)
+
+    def __str__(self):
+        if self.operand == "":
+            return f"[{self.step}] {self.opcode}{self.register.get_reg_adr():<5}{self.value:<4},  {self.five_bit_opcode():<5}{self.register.to_3_bit_adr():<7}{self.clkcyc:<5}"
+        else:
+            return f"[{self.step}] {self.opcode}{self.register.get_reg_adr():<5}{self.operand:<4},  {self.five_bit_opcode():<5}{self.register.to_3_bit_adr():<7}{self.to_16_bit_val():<22}{self.clkcyc}"
+
+
 class Registers:
     def __init__(self, reg_val, reg_adr):
-        self.reg_val = reg_val
+        self.reg_val = reg_val 
         self.reg_adr = reg_adr
 
     def set_reg_adr(self, reg_adr):
@@ -15,157 +65,128 @@ class Registers:
     def get_reg_val(self):
         return self.reg_val
 
-    def to_32bit_val(self):
+    def to_16_bit_val(self):
         if self.reg_val >= 0:
-            return f"{self.reg_val:032b}"
+            return format(self.reg_val, '016b')
         else:
-            return bin(self.reg_val & 0xFFFFFFFF)[2:]
+            return bin(self.reg_val & 0b1111111111111111)[2:].zfill(16)
 
-    def to_3bit_adr(self):
-        reg_map = {"r0": "000", "r1": "001", "r2": "010", "r3": "011", "r4": "100", "r5": "101", "r6": "110", "r7": "111"}
-        return reg_map.get(self.reg_adr, "")
-
-class Memory:
-    def __init__(self, size):
-        self.size = size
-        self.memory = [0] * size
-
-    def read_memory(self, address):
-        if 0 <= address < self.size:
-            return self.memory[address]
-        else:
-            print("Error: Invalid memory address")
-            return None
-
-    def write_memory(self, address, value):
-        if 0 <= address < self.size:
-            self.memory[address] = value
-        else:
-            print("Error: Invalid memory address")
-
-class InstructionSet:
-    def __init__(self, step, opcode, register, clkcyc, operand="", value=None):
-        self.step = step
-        self.opcode = opcode
-        self.register = register
-        self.clkcyc = clkcyc
-        self.operand = operand
-        self.value = value if value is not None else register.get_reg_val()
-
-    def five_bit_opcode(self):
-        opcode_map = {
-            "mov": "00001",
-            "add": "00010",
-            "mul": "00100",
-            "sub": "00011",
-            "div": "00101",
+    def to_3_bit_adr(self):
+        adr_map = {
+            "r0": "000",
+            "r1": "001",
+            "r2": "010",
+            "r3": "011",
+            "r4": "100",
+            "r5": "101",
+            "r6": "110",
+            "r7": "111"
         }
-        return opcode_map.get(self.opcode, "00000")
-
-    def encode_instruction(self):
-        opcode = self.five_bit_opcode()
-        operand1 = self.register.to_3bit_adr()
-        operand2 = self.to_32bit_val()#[-16:]
-        return f"[{opcode} {operand1} {operand2}]"
-
-    def to_32bit_val(self):
-        if self.value >= 0:
-            return f"{self.value:032b}"
-        else:
-            return bin(self.value & 0xFFFFFFFF)[2:]
-
-def main():
-    regs = [Registers(0, f"r{i}") for i in range(8)]
-    instructions = []
-    memory = Memory(1024)
-    print("Enter instructions(eg. mov r1 10):")
-    step = 0
-
-    while True:
-        instruction = input()
-        if instruction == "end 0 0":
-            break
-
-        opcode, operand1, operand2 = instruction.split()
-        operand2_reg = None
-        register = None
-
-        for reg in regs:
-            if reg.get_reg_adr() == operand1:
-                register = reg
-            if reg.get_reg_adr() == operand2:
-                operand2_reg = reg
-
-        if operand2_reg is None:
-            operand2_reg = Registers(int(operand2), operand2)
-
-        if operand1 != operand2:
-            operation_mapping = {
-                "mov": lambda dest, src: dest.set_reg_val(src.get_reg_val()),
-                "add": lambda dest, src: dest.set_reg_val(dest.get_reg_val() + src.get_reg_val()),
-                "sub": lambda dest, src: dest.set_reg_val(dest.get_reg_val() - src.get_reg_val()),
-                "mul": lambda dest, src: dest.set_reg_val(dest.get_reg_val() * src.get_reg_val()),
-                "div": lambda dest, src: dest.set_reg_val(dest.get_reg_val() // src.get_reg_val()),
-            }
-            operation_mapping[opcode](register, operand2_reg)
-            instructions.append(InstructionSet(
-                step, opcode, register, 1, operand2_reg.get_reg_adr(), operand2_reg.get_reg_val()))
-        step += 1
-
-    print("\n  PC     Decoded:            Encoded instructions(32-bit):          Clock Cycles\n")
-    for instruction in instructions:
-        pc_str = f"PC[{instruction.step}]->"
-        decoded_form = f"{pc_str} {instruction.opcode} {instruction.register.get_reg_adr()}"
-        if instruction.operand != "":
-            decoded_form += f" {instruction.operand}"
-        decoded_form = decoded_form.ljust(20)
-        encoded_form = instruction.encode_instruction().ljust(40)
-        print(f"{decoded_form}: {encoded_form}       {instruction.clkcyc}")
-
-    print("\nSteps of the Register\n")
-    for instruction in instructions:
-        reg_adr = instruction.register.to_3bit_adr()
-        reg_val = instruction.register.get_reg_val()
-        print(
-            f"{reg_adr} = {reg_val:2} [{instruction.register.to_32bit_val()}]")
-
-    print("\nContents of Main Memory\n")
-    for i in range(10):
-        print(f"Memory[{i}]: {memory.read_memory(i)}")
-
-    print("\nAfter the program execution contents of the registers are..........\n")
-    for reg in regs:
-        reg_adr = reg.get_reg_adr()
-        reg_val = reg.get_reg_val()
-        print(f"{reg_adr} = {reg_val:3}  [{reg.to_32bit_val()}]")
-
-    total_clk_cycles = sum(instruction.clkcyc for instruction in instructions)
-    total_instructions = len(instructions)
-    cpi = total_clk_cycles / total_instructions if total_instructions > 0 else 0
-    print("\nCPI of the program..........\n")
-    print("CPI =", cpi)
-
-    clkcys_with_pipeline = len(instructions) + 3
-    print("\nPipelined Execution of the program")
-    print("=" * 38)
-    print(f"{'':17}", end="")
-    for x in range(1, clkcys_with_pipeline + 1):
-        print(f"{x:5d}", end="")
-
-    for instruction in instructions:
-        if instruction.operand == "":
-            print(
-                f"\n{instruction.step + 1}. {instruction.opcode}{instruction.register.get_reg_adr()}{instruction.value:2}", end=" ")
-        else:
-            print(
-                f"\n{instruction.step + 1}. {instruction.opcode}{instruction.register.get_reg_adr()}{instruction.operand}", end=" ")
-        for _ in range(instruction.step + 1):
-            print(" " * 5, end="")
-        print("IF | ID | EX | WB", end="")
-
-    print(
-    f"\n\nThe program execution completed in {clkcys_with_pipeline} clock cycles under the pipeline")
+        return adr_map.get(self.reg_adr, "")
 
 
 if __name__ == "__main__":
-    main()
+    instruction = ""
+    opcode = ""
+    operand = ""
+    operand2 = ""
+    regs = [Registers(0, f"r{i}") for i in range(8)]
+    steps = []
+    step = 0
+
+    print("Enter inputs(eg. mov r1 10/ stop with [end 0 0]) : ")
+
+    while not (opcode == "end" and operand == "0" and operand2 == "0"):
+        instruction = input()
+        if instruction == "end 0 0":
+            break
+        else:
+            input_list = instruction.split(" ")
+            opcode = input_list[0]
+            operand = input_list[1]
+            operand2 = input_list[2]
+            desti_reg = None
+            for reg in regs:
+                if reg.get_reg_adr() == operand:
+                    desti_reg = reg
+                    break
+            try:
+                value = int(operand2)
+                if opcode == "mov":
+                    desti_reg.set_reg_val(value)
+                    steps.append(InstructionSet(step, opcode, desti_reg, 1))
+                elif opcode == "add":
+                    desti_reg.set_reg_val(desti_reg.get_reg_val() + value)
+                    steps.append(InstructionSet(step, opcode, desti_reg, 2))
+                elif opcode == "sub":
+                    desti_reg.set_reg_val(desti_reg.get_reg_val() - value)
+                    desti_reg.to_3_bit_adr()
+                    steps.append(InstructionSet(step, opcode, desti_reg, 3))
+                elif opcode == "mul":
+                    desti_reg.set_reg_val(desti_reg.get_reg_val() * value)
+                    steps.append(InstructionSet(step, opcode, desti_reg, 4))
+                elif opcode == "div":
+                    desti_reg.set_reg_val(desti_reg.get_reg_val() // value)
+                    steps.append(InstructionSet(step, opcode, desti_reg, 4))
+            except ValueError:
+                source_reg = None
+                for reg in regs:
+                    if reg.get_reg_adr() == operand2:
+                        source_reg = reg
+                        break
+                if opcode == "mov":
+                    desti_reg.set_reg_val(source_reg.get_reg_val())
+                    steps.append(InstructionSet(step, opcode, desti_reg, 1))
+                elif opcode == "add":
+                    desti_reg.set_reg_val(desti_reg.get_reg_val() + source_reg.get_reg_val())
+                    steps.append(InstructionSet(step, opcode, desti_reg, 2))
+                elif opcode == "sub":
+                    desti_reg.set_reg_val(desti_reg.get_reg_val() - source_reg.get_reg_val())
+                    desti_reg.to_3_bit_adr()
+                    steps.append(InstructionSet(step, opcode, desti_reg, 3))
+                elif opcode == "mul":
+                    desti_reg.set_reg_val(desti_reg.get_reg_val() * source_reg.get_reg_val())
+                    steps.append(InstructionSet(step, opcode, desti_reg, 4))
+                elif opcode == "div":
+                    desti_reg.set_reg_val(desti_reg.get_reg_val() / source_reg.get_reg_val())
+                    steps.append(InstructionSet(step, opcode, desti_reg, 4))
+        step += 1
+
+    print("\n%-12s%-19s%-28s%s" % ("  PC ", "  Decoded: ", "  Encoded instruction: ", "   Clock Cycles\n"))
+    for s in steps:
+        print("%-12s%-4s%-5s%-10s%-6s%-4s%-28s%s" % ("PC[" + str(s.get_step()) + "]->", s.get_opcode(), s.get_register().get_reg_adr(),
+                                                  s.get_value(), s.five_bit_opcode(), s.get_register().to_3_bit_adr(),
+                                                  s.to_16_bit_val(), s.get_clkcyc()))
+    
+    print("\nAfter the program execution contents of the registers are..........\n")
+    for s in steps:
+        print(f"{s.get_register().get_reg_adr()} = {s.get_value()}   [{s.get_register().to_16_bit_val()}]")
+
+    print("\nAfter the program execution values of the registers are..........\n")
+    for reg in regs:
+        print(f"{reg.get_reg_adr()}   {reg.get_reg_val()}  [{reg.to_16_bit_val()}]")
+
+    total_clk_cycs = sum(s.get_clkcyc() for s in steps)
+    cpi = total_clk_cycs / step if step else 0  
+    print("") 
+    print(f"Total clock cycles = {total_clk_cycs}, Step = {step}")
+    print("CPI of the program..........")
+    print("CPI =", cpi)
+
+    clkcys_with_pipeline = len(steps) + 3
+    print("\nPipelined Execution of the program")
+    print("=" * 38)
+    print("%17s" % "", end="")
+    for x in range(1, clkcys_with_pipeline + 1):
+        print("%5d" % x, end="")
+    for s in steps:
+        if s.get_operand() == "":
+            print(f"\n{s.get_step() + 1}. {s.get_opcode()}{s.get_register().get_reg_adr()} {s.get_value()} :", end="")
+        else:
+            print(f"\n{s.get_step() + 1}. {s.get_opcode()}{s.get_register().get_reg_adr()}{s.get_operand()} :", end="")
+        for y in range(s.get_step() + 1):
+            print("%5s" % "", end="")
+        print("IF | ID | EX | WB", end="")
+    print(
+    f"\n\nThe program execution completed in {clkcys_with_pipeline} clock cycles under the pipeline")
+
